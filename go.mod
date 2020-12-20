c54365f7b45101dc6a00c9efd407f24d7fa5fda7
Adam Thomason <hello@adamthomason.me>
2020-12-20 11:14:57 -0700

    Update module name

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/adamthomason/adamthomason-cf-templates
+module github.com/adamthomason/cf-templates
 
 go 1.15
 
-require github.com/awslabs/goformation/v4 v4.15.6
+require (
+	github.com/awslabs/goformation/v4 v4.15.6
+	github.com/spf13/cobra v1.1.1
+)
--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"flag"
-	"github.com/adamthomason/adamthomason-cf-templates/plugin"
-	"github.com/adamthomason/adamthomason-cf-templates/template"
+	"github.com/adamthomason/cf-templates/cmd"
+	"github.com/adamthomason/cf-templates/plugin"
+	"github.com/adamthomason/cf-templates/template"
+	"github.com/spf13/cobra"
 )
 
 // Main method
 func main() {
-	templateFlag := flag.String("template", "", "The template to generate")
-	flag.Parse()
+	//templateFlag := flag.String("template", "", "The template to generate")
+	//flag.Parse()
+	//
+	//cfTemplate := plugin.GetPlugin("./cf-templates/" + *templateFlag + "/" + *templateFlag + ".so")
+	//
+	//template.Render(cfTemplate.Render())
 
-	cfTemplate := plugin.GetPlugin("./cf-templates/" + *templateFlag + "/" + *templateFlag + ".so")
-
-	template.Render(cfTemplate.Render())
 }
